a1080553eb8ddb5b26185ebb25eae409ac433ebc
imustend <plochockitymon@gmail.com>
2023-10-26 01:59:47 +0200

    fix some logic

--- main.cpp
+++ main.cpp
@@ -6,7 +6,8 @@ int main(int argc, char* argv[])
     //TODO: better logging and arguments
 	auto sorter = new Sorting::Sorter("input.txt", "output.txt", 1000000);
     sorter->generate_and_sort_chunks();
-//    sorter->delete_temp_files();
+    sorter->merge_sort();
+    sorter->delete_temp_files();
     std::cout << sorter->number_of_chunks << std::endl;
 	return 0;
 }
